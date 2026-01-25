c7a9912bf296c4f5f27b9e9ebd6deabe580d38cf
cheng-chun-yuan <83824908+cheng-chun-yuan@users.noreply.github.com>
2026-01-25 14:40:10 +0800

    chore: remove sdk/dist from git and add to gitignore

--- sdk/dist/core/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Core Module - Platform-agnostic utilities
- *
- * Pure functions and clients that work everywhere:
- * - Browser
- * - Node.js
- * - React Native
- */
-export { EsploraClient, esploraTestnet, esploraMainnet, } from "./esplora";
--- sdk/dist/react/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * React Hooks for ZVault SDK
- *
- * Platform-agnostic hooks that work in both React (web) and React Native.
- */
-export { useDepositWatcher, useSingleDeposit, } from "./useDepositWatcher";
